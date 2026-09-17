6e6f151e863e3566eee29c996daad3d58a76317f
Daniel Dominic <151459204+danimpulse500@users.noreply.github.com>
2026-09-17 10:31:41 +0100

    Update form action URL for tutor registration

--- src/app/tutors-registration-form/page.tsx
+++ src/app/tutors-registration-form/page.tsx
@@ -24,7 +24,7 @@ export default function TutorsRegistrationForm() {
         
         <form
           className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8"
-          action="https://formspree.io/f/maqgklpp"
+          action="https://formspree.io/f/mbjnwzal"
           method="post"
         >
           <FormInput
