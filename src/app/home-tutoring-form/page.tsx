cdb2c7a4fddaaac47d53c699f727e288fac67675
Daniel Dominic <151459204+danimpulse500@users.noreply.github.com>
2026-09-17 10:29:10 +0100

    Update form action URL for home tutoring form

--- src/app/home-tutoring-form/page.tsx
+++ src/app/home-tutoring-form/page.tsx
@@ -25,7 +25,7 @@ export default function HomeTutoringForm() {
         
         <form
           className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8"
-          action="https://formspree.io/f/meebdply"
+          action="https://formspree.io/f/xyyronkn"
           method="post"
         >
           <FormInput
